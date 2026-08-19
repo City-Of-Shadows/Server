c41f605f4759ef83bb3244adab3022459e92b9a6
City-Of-Shadows <rizwanchandna.6.@gmail.com>
2026-08-19 20:46:50 +0500

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use("/api/users", userRoutes);
 app.use(
-cors({origin: "http://localhost:5173", credentials: true,}));
+cors({origin: "https://city-of-shadows.github.io", credentials: true,}));
 app.use(express.json());
 mongoose
   .connect(process.env.MONGO_URI)
